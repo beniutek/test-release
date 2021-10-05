49cd2a9a8a86f5580ad4df1227e87e5c2fff113e
beniutek <bbeniutek@gmail.com>
2021-10-05 15:39:58 +0200

    doc: add doctype and some docs

---
+++ src/main.ts
@@ -0,0 +1,40 @@
+/**
+ *
+ * Method takes two numeric arguments and returns their sum
+ * @param a can be any numeric format number
+ * @param b can be any numeric format number
+ *
+ *  *Example*
+ * ```typescript
+ * import { sum } from 'test-release-stuff'
+ *
+ * const result = sum(1, 2);
+ * ```
+ */
+const sum = (a: number, b: number): number => {
+  if (a && b) return a + b;
+
+  return 0;
+}
+
+/**
+ *
+ *
+ * ```typescript
+ * import { multiply } from 'test-release-stuff'
+ *
+ * const result = multiply(1, 2);
+ * ```
+ *
+ */
+
+const multiply = (a: number, b: number): number => {
+  if (a && b) return a * b;
+
+  return 0;
+}
+
+export default {
+  sum,
+  multiply
+};
